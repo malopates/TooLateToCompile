04e117136c42f4e1f90d3aabef4ce57b6050fca9
Malori ALVAREZ <malori.alvarez.etu@univ-lille.fr>
2024-12-10 18:30:43 +0100

    jai dead ça

--- src/Ennemi.java
+++ src/Ennemi.java
@@ -1,8 +1,16 @@
+import extensions.CSVFile;
+import extensions.File;
+
 class Ennemi extends Program {
     String nom;
     int affinite;
     int idxDialogue;
+    String nomJauge;
+    int tauxJauge;
     CSVFile dialogue;
-    CSVFILE questions;
+    CSVFile questions;
     File sprite;
+    int longueurSprite;
+    String description;
+
 }
--- src/TooLateToCompile.java
+++ src/TooLateToCompile.java
@@ -5,17 +5,19 @@ class TooLateToCompile extends Program {
 
 //======================================================================================
 
-
+    final CSVFile ALMEIDADODO = loadCSV("dialogues/Corle.csv",';');
     final CSVFile CORLE = loadCSV("dialogues/Corle.csv",';');
     final CSVFile CONTROLEUR = loadCSV("dialogues/Controleur.csv",';');
     final CSVFile MARSHALLNORMAND = loadCSV("dialogues/MarshallNormand.csv",';');
     final CSVFile SEC = loadCSV("dialogues/Sec.csv",';');
     final CSVFile MENU = loadCSV("menu.csv",';');
     final CSVFile OUTRO = loadCSV("outro.csv",';');
-    final CSVFile QPROGRAMMATION = loadCSV("programmation.csv",';');
-    final CSVFile QMATHS = loadCSV("maths.csv",';');
-    final CSVFile QWEB = loadCSV("web.csv",';');
+    final CSVFile QPROGRAMMATION = loadCSV("questions/programmation.csv",';');
+    final CSVFile QMATHS = loadCSV("questions/maths.csv",';');
+    final CSVFile QWEB = loadCSV("questions/web.csv",';');
     final File TITRE = newFile("titre.txt");
+    final File SCONTROLEUR = newFile("sprites/controleur.txt");
+    final CSVFile DESCRIPTIONS = loadCSV("description.csv",';');
 
     
 //======================================================================================
@@ -32,7 +34,8 @@ class TooLateToCompile extends Program {
                 
             }
             etape = "menu";
-            if(equals(etape,"menu")){
+            interfaceCombat(controleur);
+            if(equals(etape,"menu")){ //pour le menu, choix de langue, +1 quand changement langue, modulo 3
                 for(int i=0;i<12;i++)
                 println(readLine(TITRE));
                 afficherCasesMenu(selection,langue);
@@ -194,6 +197,59 @@ class TooLateToCompile extends Program {
         return contenu;
     }
 
+
+//=============================== GESTION COMBAT =================================
+
+    Ennemi newEnnemi(String nom,int tauxJauge,int idxDialogue,String nomJauge,CSVFile dialogue,CSVFile questions,File sprite,int longueurSprite,String description){
+        Ennemi en = new Ennemi();
+
+        en.nom = nom;
+        en.tauxJauge = tauxJauge;
+        en.nomJauge = nomJauge;
+        en.idxDialogue = idxDialogue;
+        en.dialogue = dialogue;
+        en.questions = questions;
+        en.sprite = sprite;
+        en.longueurSprite = longueurSprite;
+        en.description = description;
+        return en;
+    }
+    
+    Ennemi controleur = newEnnemi("Contrôleur", 0, 0,"Amende", CONTROLEUR, QMATHS, SCONTROLEUR,25,getCell(DESCRIPTIONS,0,langue));
+    //Ennemi sec = newEnnemi("M.Sec", 0, 0,"Hémmoragie", CORLE, QWEB, SSEC,22);
+    
+    int currentQuestion(Ennemi ennemi){
+        return (int) (random()*rowCount(ennemi.questions)); 
+    }
+
+    void interfaceCombat(Ennemi ennemi){
+        cadreHautBas(ennemi);
+        for(int i = 0;i<ennemi.longueurSprite;i++){
+            print("║|" + "    " + "|║");
+            print("║|" + readLine(SCONTROLEUR) + "|║");
+            if(i == 4){print("   " + ANSI_RED + ennemi.nom + ANSI_WHITE );} //mettre les éléments sur la droite sur l'affichage combat
+            if(i == 5){print("   " + ennemi.description);} 
+            if(i == 6){print("   " + "Remplissez la jauge d'" + ANSI_RED + ennemi.nomJauge + ANSI_WHITE + " à gauche pour gagner ");} 
+            if(i == 9){print("   " + getCell(QMATHS,currentQuestion(ennemi),langue*6));}//moitié d'une question (pas la place autrement)
+            if(i == 9){print("");}//autre moitié
+            if(i == 11){print("   " + "1 - " + "Placeholder" + "                          " + "2  -" + "Placeholder" );}
+            if(i == 13){print( "   " + "1 - " + "Placeholder" + "                          " + "2  -" + "Placeholder");}
+            if(i != ennemi.longueurSprite-1){println();} //pour éviter de faire une ligne de vide en bas
+        }
+        cadreHautBas(ennemi);
+    }
+
+    void cadreHautBas(Ennemi ennemi){
+        println("");
+        print("✧");
+        for(int i = 0; i<2;i++){
+            for(int j = 0; i<ennemi.longueurSprite;i++){
+               print("=-="); 
+            } 
+        }
+        print("✧");
+        println("");
+    }
 
 
 }
